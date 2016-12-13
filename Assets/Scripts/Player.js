377ce9a4dcfbba7586e46e503f5c0cbe1678b045
Richard Hidalgo <hidalgo931@gmail.com>
2016-12-12 22:50:25 -0300

    initial commit

---
+++ Assets/Scripts/Player.js
@@ -0,0 +1,37 @@
+﻿#pragma strict
+
+public var jumpSpeed: float;
+
+private var body: Rigidbody;
+private var animationManager: Animator;
+private var soundManager : SoundManager;
+private var uiManager : StateManager;
+
+function Start () {
+  this.body = this.GetComponent(Rigidbody);
+  this.animationManager = this.GetComponent(Animator);
+  this.soundManager = SoundManager.Instance();
+  this.uiManager = GameObject.Find("UI").GetComponent(StateManager);
+}
+
+function Update () {
+  if (Input.GetKeyDown("space")){
+    this.body.velocity.y = 0;
+    this.body.AddForce(0, this.jumpSpeed * Time.deltaTime, 0, ForceMode.Impulse);
+    this.animationManager.Play("Fly");
+    this.soundManager.Play(SoundManager.Clips.FLAP);
+  }
+}
+
+function OnTriggerEnter(collider : Collider) {
+
+  var tag = collider.gameObject.tag;
+
+  if(tag == "Danger"){
+    this.soundManager.Play(SoundManager.Clips.GAMEOVER);
+    uiManager.StopGame();
+  } else if(tag == "Score"){
+    this.soundManager.Play(SoundManager.Clips.SCORE);
+    uiManager.IncreaseScore();
+  }
+}
---
+++ Assets/Scripts/StateManager.js
@@ -0,0 +1,59 @@
+﻿#pragma strict
+
+import SceneManagement;
+
+public var Player : GameObject;
+
+private var isPaused:boolean;
+private var isOver:boolean;
+private var startText:GameObject;
+private var scorePanel:GameObject;
+private var panelText : UI.Text;
+private var scoreCount: int;
+private var soundManager : SoundManager;
+
+public function Start():void {
+  Time.timeScale = 0;
+  this.isPaused = true;
+  this.isOver = false;
+  this.scoreCount = 0;
+  this.startText = this.gameObject.Find("StartText");
+  this.panelText = this.gameObject.Find("ScoreCount").GetComponent(UI.Text);
+  this.scorePanel = this.gameObject.Find("ScorePanel");
+
+  this.soundManager = SoundManager.Instance();
+  this.scorePanel.SetActive(false);
+}
+
+function Update () {
+  if (this.isPaused && Input.GetKeyDown("space")){
+    if(this.isOver){
+      SceneManager.LoadScene("Main");
+    } else {
+      this.StartGame();
+    }
+  }
+}
+
+private function StartGame():void {
+  Time.timeScale = 1;
+  this.isPaused = false;
+  this.startText.SetActive(false);
+  this.scorePanel.SetActive(true);
+  this.soundManager.Play(SoundManager.Clips.MAINTHEME);
+}
+
+public function StopGame():void {
+  Time.timeScale = 0;
+  this.isPaused = true;
+  this.startText.GetComponent(UI.Text).text = String.Concat("Your score is ", this.scoreCount.ToString(), ".\nPress Space to try again.");
+  this.scorePanel.SetActive(false);
+  this.startText.SetActive(true);
+  this.isOver = true;
+  this.soundManager.Stop(SoundManager.Clips.MAINTHEME);
+}
+
+public function IncreaseScore():void {
+    this.scoreCount++;
+    this.panelText.text = this.scoreCount.ToString();
+}
